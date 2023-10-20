73a94d48b1366aacf847bd561d20244097ced378
OliverMarquezEUG <144775408+OliverMarquezEUG@users.noreply.github.com>
2023-10-20 10:10:22 +0200

    Update script.js

--- dist/object/js/script.js
+++ dist/object/js/script.js
@@ -1,1 +1,20 @@
-fgdf
+document.getElementById('btnGuardarDatos').addEventListener('click', function() {
+    var nom = prompt('Introduce tu nombre:');
+    var cognom = prompt('Introduce tu apellido:');
+    var edat = prompt('Introduce tu edad:');
+    
+    window.usuario = {
+        nom: nom,
+        cognom: cognom,
+        edat: edat
+    };
+});
+
+document.getElementById('btnMostrarDatos').addEventListener('click', function() {
+    if (window.usuario) {
+        var infoDiv = document.getElementById('infoUsuario');
+        infoDiv.innerHTML = 'Nombre: ' + window.usuario.nom + '<br>Apellido: ' + window.usuario.cognom + '<br>Edad: ' + window.usuario.edat;
+    } else {
+        alert('Primero debes guardar los datos del usuario.');
+    }
+});
